fe64bb5f414d1db0bd3dcf7bcdb7624e93453c08
phcs93 <phcs93@gmail.com>
2023-10-26 05:03:37 -0300

    display toggle for build or details

--- src/components/pokemon/pokemon.js
+++ src/components/pokemon/pokemon.js
@@ -6,22 +6,32 @@ function renderPokemon (pokemon, pokemonSettings, teamSlot, versionId) {
     const versionGroupId = Globals.Database.Versions[versionId].versionGroupId;
     return `
         <div class="pokemon flex-rows gap">
+
             ${renderPokemonHeader(pokemon, pokemonSettings, true)}
+
             <hr>
-            ${pokemonSettings.moves[0] ? renderMove(pokemonSettings.moves[0], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 1)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 1)">SELECT MOVE</button>`}
-            ${pokemonSettings.moves[1] ? renderMove(pokemonSettings.moves[1], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 2)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 2)">SELECT MOVE</button>`}
-            ${pokemonSettings.moves[2] ? renderMove(pokemonSettings.moves[2], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 3)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 3)">SELECT MOVE</button>`}
-            ${pokemonSettings.moves[3] ? renderMove(pokemonSettings.moves[3], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 4)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 4)">SELECT MOVE</button>`}
+
+            <div class="flex-rows gap pokemon-build">
+                ${pokemonSettings.moves[0] ? renderMove(pokemonSettings.moves[0], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 1)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 1)">SELECT MOVE</button>`}
+                ${pokemonSettings.moves[1] ? renderMove(pokemonSettings.moves[1], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 2)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 2)">SELECT MOVE</button>`}
+                ${pokemonSettings.moves[2] ? renderMove(pokemonSettings.moves[2], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 3)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 3)">SELECT MOVE</button>`}
+                ${pokemonSettings.moves[3] ? renderMove(pokemonSettings.moves[3], versionGroupId, `showMoveSelectorSwal(${teamSlot}, 4)`) : `<button class="select-move-button" onclick="showMoveSelectorSwal(${teamSlot}, 4)">SELECT MOVE</button>`}
+            </div>
+
+            <div class="flex-rows gap pokemon-details">
+                ${/*renderPokemonDetails(pokemon)*/"&lt;details&gt;"}
+                <hr>
+                ${renderGenderRate(pokemon)}
+                <hr>
+                ${renderStats(pokemon)}
+                <hr>
+                ${renderTypesEffectiveness(pokemon)}
+            </div>
+            
             <hr>
-            ${/*renderPokemonDetails(pokemon)*/"&lt;details&gt;"}
-            <hr>
-            ${/*renderGenderRate(pokemon)*/"&lt;gender-rate&gt;"}
-            <hr>
-            ${/*renderStats(pokemon)*/"&lt;statse&gt;"}
-            <hr>
-            ${/*renderTypesEffectiveness(pokemon)*/"&lt;type-weaknesses&gt;"}
-            <hr>
+
             <button onclick="removePokemon(${teamSlot})">REMOVE</button>
+
         </div>
     `;
 }
